c7ff14778c9df5c5e67e3774c00363507f4ecddc
DionisioGames <dionisiobrustolin24@gmail.com>
2026-09-17 16:40:30 -0400

    Implemantei Senhas HASHS e JWT no codigo, acrescentando tambem HTTPBearer.

--- main/security.py
+++ main/security.py
@@ -1,5 +1,13 @@
 from pwdlib import PasswordHash
 
+import jwt 
+from datetime import datetime, timedelta, timezone
+
+
+from fastapi import Depends, HTTPException
+from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
+
+
 password_hash = PasswordHash.recommended()
 
 
@@ -9,3 +17,60 @@ def gerar_hash_senha(senha: str):
 
 def verificar_senha(senha: str, senha_hash: str):
     return password_hash.verify(senha, senha_hash)
+
+#JWT
+SECRET_KEY = "minha-chave-secreta"
+ALGORITHM = "HS256"
+
+security = HTTPBearer()
+
+
+def criar_token(id_usuario: int, tipo: str):
+    expiracao = datetime.now(timezone.utc) + timedelta(minutes=30)
+
+    dados_token = {
+        "sub": str(id_usuario),
+        "tipo": tipo,
+        "exp": expiracao
+    }
+
+    token = jwt.encode(
+        dados_token, #dados do token
+        SECRET_KEY, #minha chave secreta
+        algorithm=ALGORITHM
+    )
+
+    return token
+
+def verificar_token(token: str):
+    try: 
+        dados = jwt.decode(
+            token,
+            SECRET_KEY,
+            algorithms=[ALGORITHM]
+        )
+
+        return dados
+    
+    except jwt.ExpiredSignatureError:
+        # Token está invalido por ter expirado
+        return None
+
+    except jwt.InvalidTokenError:
+        # Token está errado ou foi mexido
+        return None
+
+def usuario_autenticado(
+        credenciais: HTTPAuthorizationCredentials = Depends(security)
+): 
+    token = credenciais.credentials
+
+    dados = verificar_token(token)
+
+    if dados is None:
+        raise HTTPException(
+            status_code = 401, #401 Unauthorized Usuário não está autenticado ou login está incorreto
+            detail = "Token inválido ou expirado. Faça o Login novamente."
+        )
+    return dados
+
--- routes/auth/auth_routes.py
+++ routes/auth/auth_routes.py
@@ -1,10 +1,10 @@
-from fastapi import APIRouter, HTTPException
+from fastapi import APIRouter, HTTPException, Depends
 
 from main.models import Login
 
 from main.data import usuarios
 
-from main.security import verificar_senha
+from main.security import verificar_senha, criar_token, usuario_autenticado
 
 router = APIRouter(tags = ["Autenticado"])
 
@@ -16,8 +16,15 @@ def login(dados: Login):
             dados.senha,  # senha digitada pelo usuário
             usuario.senha # senha armazenada no banco de dados(HASH)  
             ):
-            return {
+
+                token = criar_token(
+                     usuario.id, # id da poha do usuario
+                     usuario.tipo # tipo do usuario(professor ou secretaria)
+                )
+
+                return {
                 "mensagem": "Login realizado com sucesso",
+                "token": token,
                 "usuario": {
                     "id": usuario.id,
                     "nome": usuario.nome,
@@ -30,3 +37,26 @@ def login(dados: Login):
         status_code = 401, #401 Unauthorized Usuário não está autenticado ou login está incorreto
         detail = "Erro ao realizar login. Verifique os dados e tente novamente."
     ) 
+
+@router.get("/teste-protegido")
+def teste_protegido(
+     dados_token: dict = Depends(usuario_autenticado) # O fastapi vai chamar a função usuario_autenticado e vai passar o resultado dela para a variavel dados_token
+):
+     return {
+          "mensagem": "Você está autenticado e pode acessar essa rota",
+          "dados_token": dados_token 
+     }
+
+@router.get("/teste-secretaria")
+def teste_secretaria(
+    dados_token: dict = Depends(usuario_autenticado) # Chama a função usuario_autenticado.
+):
+    if dados_token["tipo"] != "secretaria": #Exemplo de como verificar o tipo do usuário. Se o tipo do usuário não for "secretaria", ele não terá acesso a essa rota.
+        raise HTTPException(
+            status_code=403,
+            detail="Apenas a secretaria pode acessar esta rota."
+        )
+
+    return {
+        "mensagem": "Acesso autorizado para a secretaria!"
+    }
